b61753f0b9601f80ec1d0dcab1c535845ec04f34
fatih Yılmaz <fatikyilmaz@hotmail.com>
2021-11-30 15:38:01 +0300

    create mainPanel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentTablesComponent } from './component-tables/component-tables.component';
+import { TableDetailComponent } from './table-detail/table-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ComponentTablesComponent
+    ComponentTablesComponent,
+    TableDetailComponent
   ],
   imports: [
     BrowserModule,
