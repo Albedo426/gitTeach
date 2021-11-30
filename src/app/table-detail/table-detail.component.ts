41477016bd5781ea30443c5fc541ad2af0d2c8bf
fatih Yılmaz <fatikyilmaz@hotmail.com>
2021-11-30 16:14:29 +0300

    create TableDetail

--- src/app/table-detail/table-detail.component.ts
+++ src/app/table-detail/table-detail.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-table-detail',
   templateUrl: './table-detail.component.html',
   styleUrls: ['./table-detail.component.css']
 })
+
 export class TableDetailComponent implements OnInit {
 
   constructor() { }
